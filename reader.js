1e4c05a0520fc993ee7ec94634e008bfe818ff65
Rob Emanuele <rob@emanuele.us>
2012-09-29 17:23:01 -0700

    use javascript to emulate dropdown fuctionality

---
+++ reader.js
@@ -0,0 +1,4 @@
+function set_category_value(category, anchor) {
+    document.getElementById(category + 'btn').innerHTML = anchor.innerHTML;
+    document.getElementById(genre + 'field').innerHTML = this.innerHTML;
+}
