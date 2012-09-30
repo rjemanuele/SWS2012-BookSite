d0d9df426b8a4cc65d71085b8366c0e0efe5c3d2
Rob Emanuele <rob@emanuele.us>
2012-09-30 11:30:46 -0700

    Create drop downs for resetting the preference

--- book_genie/search/static/reader.js
+++ book_genie/search/static/reader.js
@@ -1,8 +1,11 @@
-function set_category_value(category, anchor, value) {
-    if (arguments.length == 2) {
+function set_category_value(category, anchor, value, text_field_value) {
+    if (arguments.length < 4) {
+	text_field_value = anchor.innerHTML;
+    }
+    if (arguments.length < 3) {
 	value = anchor.innerHTML;
     }
-    document.getElementById(category + '_btn').innerHTML = anchor.innerHTML;
-    document.getElementById(category + '_text_field').value = anchor.innerHTML;
+    document.getElementById(category + '_btn').innerHTML = text_field_value + '<i class="icon-chevron-down"></i>';
+    document.getElementById(category + '_text_field').value = text_field_value;
     document.getElementById(category + '_field').value = value;
 }
