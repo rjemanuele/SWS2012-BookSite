85261f0e4f52b39b9a6f6bce95fbfe95fca29794
Rob Emanuele <rob@emanuele.us>
2012-09-29 18:03:14 -0700

    cleaner drop down functionality
    temporary result data

--- reader.js
+++ reader.js
@@ -1,4 +1,7 @@
-function set_category_value(category, anchor) {
-    document.getElementById(category + 'btn').innerHTML = anchor.innerHTML;
-    document.getElementById(genre + 'field').innerHTML = this.innerHTML;
+function set_category_value(category, anchor, value) {
+    if (arguments.length == 2) {
+	value = anchor.innerHTML;
+    }
+    document.getElementById(category + '_btn').innerHTML = value;
+    document.getElementById(category + '_field').value = value;
 }
